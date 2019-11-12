c2d433fe856f6d3d06db14c80bd391d0cf8e3db0
Paulo R. A. Sales <paulo.sales@intelligir.com.br>
2019-11-12 16:14:22 -0300

    Removing exeeded parameter into method to update camunda variables

--- src/app/camunda/core.js
+++ src/app/camunda/core.js
@@ -1,95 +1,108 @@
-'use strict'
+"use strict";
 
 const convertToCamundaVariables = vars => {
-    const keys = Object.keys(vars)
-    const variables = {}
+  const keys = Object.keys(vars);
+  const variables = {};
 
-    for (let k in keys) {
-        if (k === 'businessKey') {
-            continue
-        }
-
-        const value = !vars[keys[k]] 
-                        ? 'null' 
-                        : vars[keys[k]].toString().substr(0, 4000)
-
-        variables[`${keys[k]}`] = {
-            value,
-            type: 'String'
-        }
+  for (let k in keys) {
+    if (k === "businessKey") {
+      continue;
     }
 
-    return variables
-}
+    const value = !vars[keys[k]]
+      ? "null"
+      : vars[keys[k]].toString().substr(0, 4000);
+
+    variables[`${keys[k]}`] = {
+      value,
+      type: "String"
+    };
+  }
+
+  return variables;
+};
 
 module.exports = instance => {
-    return {
-        // Tasks
-        getTasksByDefinitionKey: async (tenantId, processDefinitionKey, taskDefinitionKey) => {
-            return await instance.get(
-                `/engine-rest/task?tenantIdIn=${tenantId}&processDefinitionKey=${processDefinitionKey}&taskDefinitionKey=${taskDefinitionKey}`
-            )
-        },
-        doTaskClaim: async (taskId, username) => {
-            return await instance.post(`/engine-rest/task/${taskId}/claim`, { userId: username })
-        },
-        getTaskVariables: async taskId => {
-            const resVariables = await instance.get(`/engine-rest/task/${taskId}/variables`)
-            const variables = resVariables.data
+  return {
+    // Tasks
+    getTasksByDefinitionKey: async (
+      tenantId,
+      processDefinitionKey,
+      taskDefinitionKey
+    ) => {
+      return await instance.get(
+        `/engine-rest/task?tenantIdIn=${tenantId}&processDefinitionKey=${processDefinitionKey}&taskDefinitionKey=${taskDefinitionKey}`
+      );
+    },
+    doTaskClaim: async (taskId, username) => {
+      return await instance.post(`/engine-rest/task/${taskId}/claim`, {
+        userId: username
+      });
+    },
+    getTaskVariables: async taskId => {
+      const resVariables = await instance.get(
+        `/engine-rest/task/${taskId}/variables`
+      );
+      const variables = resVariables.data;
 
-            if (!variables) {
-                return { data: {} }
-            }
+      if (!variables) {
+        return { data: {} };
+      }
 
-            const vars = {}
-            const keys = Object.keys(variables)
+      const vars = {};
+      const keys = Object.keys(variables);
 
-            for (let k in keys) {
-                vars[`${keys[k]}`] = variables[`${keys[k]}`].value
-            }
+      for (let k in keys) {
+        vars[`${keys[k]}`] = variables[`${keys[k]}`].value;
+      }
 
-            return { data: vars }
-        },
-        doTaskComplete: async (taskId, vars) => {
-            const keys = Object.keys(vars)
-            const variables = {}
+      return { data: vars };
+    },
+    doTaskComplete: async (taskId, vars) => {
+      const keys = Object.keys(vars);
+      const variables = {};
 
-            for (let k in keys) {
-                const value = !vars[keys[k]] ? 'null' : vars[keys[k]].toString().substr(0, 4000)
-                variables[`${keys[k]}`] = {
-                    value,
-                    type: 'String'
-                }
-            }
+      for (let k in keys) {
+        const value = !vars[keys[k]]
+          ? "null"
+          : vars[keys[k]].toString().substr(0, 4000);
+        variables[`${keys[k]}`] = {
+          value,
+          type: "String"
+        };
+      }
 
-            return await instance.post(`/engine-rest/task/${taskId}/complete`, { variables })
-        },
-        // Process
-        getProcessInstanceByTaskId: async taskId => {
-            const resTask = await instance.get(`/engine-rest/task/${taskId}`)
-            const task = resTask.data
-            const resInstance = await instance.get(
-                `/engine-rest/process-instance/${task.processInstanceId}`
-            )
-            return resInstance
-        },
-        doUpdateVariables: async (tenantId, taskId, vars) => {
-            const variables = convertToCamundaVariables(vars)
+      return await instance.post(`/engine-rest/task/${taskId}/complete`, {
+        variables
+      });
+    },
+    // Process
+    getProcessInstanceByTaskId: async taskId => {
+      const resTask = await instance.get(`/engine-rest/task/${taskId}`);
+      const task = resTask.data;
+      const resInstance = await instance.get(
+        `/engine-rest/process-instance/${task.processInstanceId}`
+      );
+      return resInstance;
+    },
+    doUpdateVariables: async (taskId, vars) => {
+      const variables = convertToCamundaVariables(vars);
 
-            return await instance.post(
-                `/engine-rest/task/${taskId}/variables`, variables
-            )
-        },
-        doStart: async (tenantId, processKey, vars) => {
-            const variables = convertToCamundaVariables(vars)
+      return await instance.post(
+        `/engine-rest/task/${taskId}/variables`,
+        variables
+      );
+    },
+    doStart: async (tenantId, processKey, vars) => {
+      const variables = convertToCamundaVariables(vars);
 
-            return await instance.post(
-                `/engine-rest/process-definition/key/${processKey}/tenant-id/${tenantId}/start`,
-                {
-                    businessKey: vars.businessKey,
-                    variables
-                }
-            )
+      return await instance.post(
+        `/engine-rest/process-definition/key/${processKey}/tenant-id/${tenantId}/start`,
+        {
+          businessKey: vars.businessKey,
+          variables
         }
+      );
     }
-}
+  };
+};
